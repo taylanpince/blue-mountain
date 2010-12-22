4b0ed3f7029577f3d8c86dcccf86ef7697c655b6
Taylan Pince <taylanpince@gmail.com>
2010-12-22 15:36:24 +0200

    Added final illustrations and flash pieces, implemented template tag for passing next flight's start date to the flash piece

--- build/bluemountain/contests/templatetags/contest_tags.py
+++ build/bluemountain/contests/templatetags/contest_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from datetime import datetime, timedelta
 
 from django import template
@@ -39,3 +41,40 @@ def countdown(target_date):
         "hours": hours,
         "minutes": minutes,
     }
+
+
+class NextContestNode(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+
+    def render(self, context):
+        try:
+            contest = Contest.objects.filter(
+                start_date__gt=datetime.now(),
+            ).order_by("start_date")[0]
+        except IndexError:
+            contest = None
+
+        context[self.var_name] = contest
+
+        return ""
+
+
+@register.tag
+def next_contest(parser, token):
+    """
+    Gets next contest and injects it into the template context
+    """
+    try:
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
+
+    m = re.search(r'as (\w+)', arg)
+
+    if not m:
+        raise template.TemplateSyntaxError, "%r had invalid arguments" % tag_name
+
+    var_name, = m.groups()
+
+    return NextContestNode(var_name)
